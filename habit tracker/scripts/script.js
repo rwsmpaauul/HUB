6cfc64e0a47bdcaa35625dc2be2608d900fb6e81
PaulRitz <ritzpaul2307schule@gmx.de>
2026-02-18 13:37:40 +0100

    add supaBase

--- habit tracker/scripts/script.js
+++ habit tracker/scripts/script.js
@@ -1,17 +1,26 @@
 import * as updateStats from "./PieCharts.js";
 import * as time from "./time.js";
 import * as createElements from "./createElements.js";
+import * as cloud from "./supaBase.js";
 
 const habits = ["Bett gemacht", "Übungsaufgaben", "Sport gemacht", "Stoff aufgearbeitet"];
 const container = document.getElementById("habit_table");
+createElements.generateTable(container, habits);
 
-document.addEventListener('change', (event) => {
+async function init() {
+    await cloud.readFromCloud();
+    updateStats.updatePieChart();
+    time.highlight_wday();
+}
+
+init();
+
+document.addEventListener('change', async (event) => {
     if (event.target.type === 'checkbox') {
         updateStats.updatePieChart();
+        await cloud.saveToCloud();
     }
 });
 
 
-createElements.generateTable(container, habits);
-updateStats.updatePieChart();
-time.highlight_wday();
+
---
+++ habit tracker/scripts/supaBase.js
@@ -0,0 +1,37 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseURL = 'https://xxegitaqnudhmrkbyxyl.supabase.co';
+const supabaseKEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4ZWdpdGFxbnVkaG1ya2J5eHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MTA0ODUsImV4cCI6MjA4Njk4NjQ4NX0.lOvRgHu8c6Z4VWw9yYzvfUn6puF81wPIClT6gfSD5_8'
+
+export const supabase = createClient(supabaseURL, supabaseKEY);
+
+export async function saveToCloud() {
+    const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+    const status = Array.from(checkboxes).map(cb => cb.checked);
+
+    const { error } = await supabase
+        .from('habits')
+        .upsert(
+            { id: 1, states: status },
+            { onConflict: 'id' }
+        )
+
+    if (error) console.error('Fehler beim Speichern:', error);
+}
+
+export async function readFromCloud() {
+    const { data, error } = await supabase
+        .from('habits')
+        .select('states')
+        .eq('id', 1)
+        .single();
+
+    if (data && data.states) {
+        const checkboxes = document.querySelectorAll('table input[type="checkbox"]');
+        checkboxes.forEach((cb, index) => {
+            if (data.states[index] !== undefined) {
+                cb.checked = data.states[index];
+            }
+        })
+    }
+}
